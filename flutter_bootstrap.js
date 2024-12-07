be8bd0be41cf27ffe19cc4f1f1b4a710d3d3d0e5
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-12-07 23:23:57 +0200

    Add flutter Native Splash & Some Assets.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2512525050"
+    serviceWorkerVersion: "849327264"
   }
 });
