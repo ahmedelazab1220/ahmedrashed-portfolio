03a37fbeaa76397a711d6cb5c94587dabd293795
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-12-08 01:26:55 +0200

    Add About Screen.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2703468345"
+    serviceWorkerVersion: "366015673"
   }
 });
